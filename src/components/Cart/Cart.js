dc0f3902da357c7dd5690c918f906de02958a95c
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-09-25 23:21:04 +0600

    cart updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const { cart } = props;
+
+    console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = Number((total * 0.1).toFixed(2));
+
+    const grandTotal = total + shipping + tax;
+
+
     return (
-        <div>
-            <h3>Order Summary</h3>
-            <p>Selected items: {props.cart.length}</p>
+        <div className='cart'>
+            <h3 className='cart-title'>Order Summary</h3>
+
+            <div className="cart-info">
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <p className='total'>Grand Total: ${grandTotal}</p>
+            </div>
+
+            <div className="action-button">
+                <button className='clear'>Clear Cart</button>
+                <button className='review'>Review Order</button>
+            </div>
         </div>
     );
 };
